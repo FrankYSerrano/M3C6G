f864fa8aefe85e047b2230b435297a376fdb1375
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-29 15:26:33 -0300

    avanzando

--- archivos_multiformato.py
+++ archivos_multiformato.py
@@ -1,121 +1,132 @@
 import csv
-import clases as c
-import funciones as f
-import random
-#import xml.sax.handler
-#import xml.sax
+import os
+from xml.etree.ElementTree import parse
+import json
 
-#punto 3 y 4: Carga de datos a variables
+#import pandas as pd
+#import clases as c
+#import funciones as f
+
+#Punto 1: Se tomo el precio del dolar observado de Banco Central de Chil
+#Punto 2: Se salvo esa matriz en los diferentes formatos requeridos
+#Punto 3: Carga de datos a variables
 with open("data\datos1.xml") as datos:
     datos1 = datos.read() 
-#print(datos1)
 with open("data\datos2.json") as datos:
     datos2 = datos.read() 
-#print(datos2)
-with open("data\datos3.csv", "r") as datos:
-#    datos3 = datos.read()
-    datos3 = csv.reader(datos)
-    for linea in datos3:
-        print(linea)
-
-with open("data\datos8.csv", mode="w") as datos:
-#    datos3 = datos.write()
-#    encabezados = csv.writer(datos, delimiter=',')
-    encabezados = ['EncA', 'EncB', 'EncC']
-    writer = csv.DictWriter(datos, fieldnames = encabezados)
-    writer.writeheader()
-    writer.writerow({"EncA": "ABC", "EncB": "PEPE", "EncC": "xxx"})
-    writer.writerow({"EncA": "DEF", "EncB": "FAFA", "EncC": "yyy"})
-#    datosx.writerrow()
-#    datos3 = csv.reader(datos)
-#    for linea in datosx:
-#        print(linea)
-
-
-#print(datos3)
+with open("data\datos3.csv") as datos:
+    datos3 = datos.read()
 with open("data\datos4.txt") as datos:
     datos4 = datos.read() 
+
+#Punto 4: Impresion tal como se cargaron
+#print(datos1)
+#print(datos2)
+print(datos3)
 #print(datos4)
 
-#Punto 5: Unificar todos los datos en una sola estructura 
+#Punto 5: Unificacion en datos_total
+os.system("cls")
 
-#Ciclo que recorra datos1 XML 
-# usando getElementsByTagName(NOMBRE) + getElementsByTagName(APELLIDO) + getElementsByTagName(OTRO)
-# Append a "datos_total"
+#Se almacenara todo en datos_total
+datos_total = {'Día':[], 'Enero':[], 'Febrero':[], 'Marzo':[], 'Abril':[], 'Mayo':[],
+                'Junio':[], 'Julio':[], 'Agosto':[], 'Septiembre':[], 
+                'Octubre':[], 'Noviembre':[], 'Diciembre':[]}
 
-#Ciclo que recorra datos2 JSON/DICCIONARIO 
-# buscando por ID y extrayendo el valor de (NOMBRE) + (APELLIDO) + (OTRO)
-# Append a "datos_total"
+#PROCESAR XML
+document = parse('data\datos1.xml')
+i=1
+for item in document.iterfind('row'):
+    datos_total['Día'].append(item.findtext('Día'))
+    datos_total['Enero'].append(item.findtext('Enero'))
+    datos_total['Febrero'].append(item.findtext('Febrero'))
+    datos_total['Marzo'].append(item.findtext('Marzo'))
+    datos_total['Abril'].append(item.findtext('Abril'))
+    datos_total['Mayo'].append(item.findtext('Mayo'))
+    datos_total['Junio'].append(item.findtext('Junio'))
+    datos_total['Julio'].append(item.findtext('Julio'))
+    datos_total['Agosto'].append(item.findtext('Agosto'))
+    datos_total['Septiembre'].append(item.findtext('Septiembre'))
+    datos_total['Octubre'].append(item.findtext('Octubre'))
+    datos_total['Noviembre'].append(item.findtext('Noviembre'))
+    datos_total['Diciembre'].append(item.findtext('Diciembre'))
+#    print("Inclui la linea: ", i)
+    i=i+1
 
-#Ciclo que recorra datos3 CSV 
-# Usando linea.split(",") y extrayendo los elementos (NOMBRE) + (APELLIDO) + (OTRO) asociados a de la lista generada
-# Append a "datos_total"
+#PROCESAR CSV
+with open("data\datos3.csv") as datos:
+    reader = csv.reader(datos)
+    i = 1
+    for linea in reader:
+        if i > 2:
+            datos_total['Día'].append(linea[0])
+            datos_total['Enero'].append(linea[1])
+            datos_total['Febrero'].append(linea[2])
+            datos_total['Marzo'].append(linea[3])
+            datos_total['Abril'].append(linea[4])
+            datos_total['Mayo'].append(linea[5])
+            datos_total['Junio'].append(linea[6])
+            datos_total['Julio'].append(linea[7])
+            datos_total['Agosto'].append(linea[8])
+            datos_total['Septiembre'].append(linea[9])
+            datos_total['Octubre'].append(linea[10])
+            datos_total['Noviembre'].append(linea[11])
+            datos_total['Diciembre'].append(linea[12])
+        i = i + 1
 
-#Ciclo que recorra datos3 TXT 
-# Usando linea.split(" ") y extrayendo los elementos (NOMBRE) + (APELLIDO) + (OTRO) asociados a de la lista generada
-# Append a "datos_total"
+#PROCESAR TXT
+with open("data\datos4.csv") as datos:
+    reader = csv.reader(datos)
+    i = 1
+    for linea in reader:
+        if i > 2:
+            datos_total['Día'].append(linea[0])
+            datos_total['Enero'].append(linea[1])
+            datos_total['Febrero'].append(linea[2])
+            datos_total['Marzo'].append(linea[3])
+            datos_total['Abril'].append(linea[4])
+            datos_total['Mayo'].append(linea[5])
+            datos_total['Junio'].append(linea[6])
+            datos_total['Julio'].append(linea[7])
+            datos_total['Agosto'].append(linea[8])
+            datos_total['Septiembre'].append(linea[9])
+            datos_total['Octubre'].append(linea[10])
+            datos_total['Noviembre'].append(linea[11])
+            datos_total['Diciembre'].append(linea[12])
+        i = i + 1
+
+print(datos_total['Día'])
+
+
+
 '''
-from xml.etree.ElementTree import parse
-xml_docx = parse('data\profe.xml')
-#print(xml_doc)
-print(dir(xml_docx))
-#print(help(xml_docx))
-a = xml_docx.itemfind("Nombre")
-print(a)
+#PROCESAR JSON
+document2 = open('data\datos2.json')
 
-#b = xml_docx.findtext('Nombre')
-#print(b)
-
-#for ele in xml_doc.find('titulo'):
-#for ele in xml_doc.findtext('titulo'):
-#    print(ele.text)
-
-print("termine")
-
-class AlbumSaxHandler(xml.sax.handler.ContentHandler):
-    def __init__(self):
-        self.in_title = False
-    def startElement(self, name, attributes):
-        if name == 'titulo':
-            self.in_title = True
-    def characters(self, data):
-        if self.in_title:
-            print(data)
-    def endElement(self, name):
-        if name == 'titulo':
-            self.in_title = False
-
-parser = xml.sax.make_parser()
-sax_handler = AlbumSaxHandler()
-parser.setContentHandler(sax_handler)
-parser.parse(datos1)
-
+linea = document2.readline()
+lineaT=""
+for linea in document2:
+#    print(linea)
+    lineaT = lineaT + linea
+#    print("***", data)
+#print(lineaT)
+#data = json.loads(lineaT)
+'''
 '''
 
 
-
-#datos_total = datos1 + datos2 + datos3 + datos4
-#print(datos_total)
+document2.close()
 
 
 
-'''
-5. Unificar todos los datos en una sola estructura llamada datos_total.
-Para esto deberá tener cuidado de no mezclar nombres de columnas
-entre los distintos conjuntos de datos provenientes de diferentes
-archivos.
-6. Mostrar los datos en pantalla con aspecto de tabla con títulos de
-columnas.
-7. Realizar una copia de los datos a otra variable que contenga solo las
-primeras 4 columnas de los datos.
-8. Cree 3 nuevas columnas, a la derecha de las 4 existentes, llamadas
-‘max’, ‘min’, ‘promedio’.
-9. Llene esas columnas con los valores máximos, mínimos y promedio
-de cada fila, respectivamente.
-10. Calcule y muestre en pantalla el valor máximo, mínimo y
-promedio de cada una de las primeras 4 columnas.
-11. Muestre en pantalla la tabla completa actualizada.
-12. Guarde los datos resultantes en 4 archivos diferentes, con
-formatos XML, JSON, CSV y Separado por expacios.
-13. Deje sus resultados en el repositorio GitHub del ejercicio.
+
+#print(datos_total['Día'])
+
+
+
+# Cierre de Archivos
+#datos1.close()
+#datos2.close()
+#datos3.close()
+#datos4.close()
 '''
