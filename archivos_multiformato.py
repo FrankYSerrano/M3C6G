906abba877e80ac067e8387613bb0009946a263f
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-30 20:45:16 -0300

    FINAL

--- archivos_multiformato.py
+++ archivos_multiformato.py
@@ -22,7 +22,7 @@ with open("data\datos4.txt") as datos:
 #Punto 4: Impresion tal como se cargaron
 #print(datos1)
 #print(datos2)
-print(datos3)
+#print(datos3)
 #print(datos4)
 
 #Punto 5: Unificacion en datos_total
@@ -53,6 +53,28 @@ for item in document.iterfind('row'):
 #    print("Inclui la linea: ", i)
     i=i+1
 
+#PROCESAR JSON
+with open("data\datos2.json") as datos:
+    datos2 = datos.read() 
+data = json.loads(datos2)
+
+i = 1
+#for linea in document2:
+for i in range(1, 9):
+    datos_total['Día'].append(data[i][''])
+    datos_total['Enero'].append(data[i]['__1'])
+    datos_total['Febrero'].append(data[i]['__2'])
+    datos_total['Marzo'].append(data[i]['__3'])
+    datos_total['Abril'].append(data[i]['__4'])
+    datos_total['Mayo'].append(data[i]['__5'])
+    datos_total['Junio'].append(data[i]['__6'])
+    datos_total['Julio'].append(data[i]['__7'])
+    datos_total['Agosto'].append(data[i]['__8'])
+    datos_total['Septiembre'].append(data[i]['__9'])
+    datos_total['Octubre'].append(data[i]['__10'])
+    datos_total['Noviembre'].append(data[i]['__11'])
+    datos_total['Diciembre'].append(data[i]['__12'])
+
 #PROCESAR CSV
 with open("data\datos3.csv") as datos:
     reader = csv.reader(datos)
@@ -76,10 +98,10 @@ with open("data\datos3.csv") as datos:
 
 #PROCESAR TXT
 with open("data\datos4.txt") as datos:
-    reader = csv.reader(datos,delimiter='alt+09')
+    reader = csv.reader(datos,delimiter='.')
     i = 1
     for linea in reader:
-        if i > 2:
+        if i > 1:
             datos_total['Día'].append(linea[0])
             datos_total['Enero'].append(linea[1])
             datos_total['Febrero'].append(linea[2])
@@ -95,38 +117,36 @@ with open("data\datos4.txt") as datos:
             datos_total['Diciembre'].append(linea[12])
         i = i + 1
 
-print(datos_total['Día'])
+for i in range(0, 31):
+    print(datos_total['Día'][i] , "-" ,
+            datos_total['Enero'][i] , "-" , 
+            datos_total['Febrero'][i] , "-" , 
+            datos_total['Marzo'][i] , "-" , 
+            datos_total['Abril'][i] , "-" , 
+            datos_total['Mayo'][i] , "-" ,
+            datos_total['Junio'][i] , "-" , 
+            datos_total['Julio'][i] , "-" , 
+            datos_total['Agosto'][i] , "-" ,
+            datos_total['Septiembre'][i] , "-" ,
+            datos_total['Octubre'][i] , "-" ,
+            datos_total['Noviembre'][i] , "-" ,
+            datos_total['Diciembre'][i])
 
+#Punto 12: Salvar en diferentes formatos
 
+#f_nuevo = open("data\newJSON.json", "w")
+#f_nuevo.close()
 
-'''
-#PROCESAR JSON
-document2 = open('data\datos2.json')
+DiccionarioAString = json.dumps(datos_total)
 
-linea = document2.readline()
-lineaT=""
-for linea in document2:
-#    print(linea)
-    lineaT = lineaT + linea
-#    print("***", data)
-#print(lineaT)
-#data = json.loads(lineaT)
-'''
-'''
+#Creacion de TXT
+fich = open("texto.txt", "w")
+fich.write(DiccionarioAString)
+#fich.write("Segunda línea\n")
+fich.close()
 
-
-document2.close()
-
-
-
-
-#print(datos_total['Día'])
-
-
-
-# Cierre de Archivos
-#datos1.close()
-#datos2.close()
-#datos3.close()
-#datos4.close()
-'''
+#Creacion de CSV
+fich = open('archCSV.csv', 'w')
+fich_w = csv.writer(fich, delimiter=',')
+fich_w.writerow(DiccionarioAString)
+fich.close()
